d25886c4209328dda593da064c3106dd2906fa87
Vadosik653 <105446055+Vadosik653@users.noreply.github.com>
2023-03-05 17:08:42 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Превет, я знаю твоё имя <?php echo $_GET["name"]; ?><br>
+    А ещё почту, Ы <?php echo $_GET["email"]; ?> <br>
+    Пароль от акков <?php echo $_GET["pass"]; ?>
+</body>
+</html>
